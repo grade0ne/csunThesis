4959730c20e4a6a84db3d0c93c95bc78a912b0ff
grade0ne <alex.mendelson.698@my.csun.edu>
2025-04-12 15:24:54 -0700

    tempResponse script added, d 18 tempRes2 data

--- scripts/tempRes2.R
+++ scripts/tempRes2.R
@@ -3,8 +3,10 @@ library(ggplot)
 
 data <- read.csv("data/tempRes2.csv")
 
+data$CloneID <- paste(data$Site, data$Leaf, data$Clone, sep = "-")
+
 graphData <- data %>%
-  filter(Day == 15) %>%
+  filter(Day == 18) %>%
   group_by(Clone, Treatment) %>%
   summarize(mean = mean(Count),
             se = sd(Count)/sqrt(length(Count)),
@@ -17,7 +19,7 @@ ggplot(data = graphData, aes(x = Clone, y = mean, fill = Treatment)) +
   geom_bar(stat = "identity", position = position_dodge(dodgeWidth), width = 0.6) +
   geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = 0.2, position = position_dodge(dodgeWidth)) +
   theme_minimal() +
-  labs(y = "Rotifers (day 15) +/-SE") +
+  labs(y = "Rotifers (day 18) +/-SE") +
   scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         panel.grid = element_blank(),
