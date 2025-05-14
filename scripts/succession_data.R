bcd68e3236e1bef4ecadbf585e5ccaf9b3901f77
grade0ne <alex.mendelson.698@my.csun.edu>
2025-05-13 22:27:43 -0700

    initial mixed genotype culture counts, day 3

--- scripts/succession_data.R
+++ scripts/succession_data.R
@@ -43,13 +43,47 @@ leaf_age <- data %>%
   group_by(Plant.ID.Numeric) %>%
   summarise(maxAge = max(leaf_age.days.))
 
-# switch from doing plant ID numeric to band number!
+# using subset data
+subset <- read.csv("data/sp_subset.csv")
 
-max_vol <- data %>%
-  filter(!is.na(Volume..mL.)) %>%
-  mutate(Band.. = as.factor(Band..),
-         Volume..mL. = as.numeric(Volume..mL.)) %>%
-  group_by(Band..) %>%
+subset <- subset %>%
+  mutate(Rotifers = as.logical(Rotifers))
+
+max_age <- subset %>%
+  group_by(Leaf) %>%
+  summarise(max_age = max(Age))
+
+max_vol <- subset %>%
+  filter(Volume > 0) %>%
+  group_by(Leaf) %>%
+  slice_max(order_by = Volume, n = 1, with_ties = F) %>%
+  left_join(max_age, by = "Leaf") %>%
+  mutate(age_perc = (Age / max_age) * 100) %>%
+  select(Leaf, Age, Volume, max_age, age_perc) %>%
+  mutate(group = "All")
+
+
+max_vol_r <- subset %>%
+  filter(Volume > 0, Rotifers == TRUE) %>%
+  group_by(Leaf) %>%
+  slice_max(order_by = Volume, n = 1, with_ties = F) %>%
+  left_join(max_age, by = "Leaf") %>%
+  mutate(age_perc = (Age / max_age) * 100) %>%
+  select(Leaf, Age, Volume, max_age, age_perc) %>%
+  mutate(group = "Rotifers")
+
+vol_graph <- bind_rows(max_vol, max_vol_r)
+vol_graph <- vol_graph %>%
+  group_by(group) %>%
   summarise(
-    maxVol = max(Volume..mL., na.rm = TRUE)
+    Volume = Volume,
+    meanVol = mean(Volume),
+    seV = sd(Volume)/sqrt(n()),
+    Age = mean(Age),
+    seA = sd(Age)/sqrt(n())
   )
+
+ggplot(vol_graph, aes(x = group, y = meanVol)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  geom_errorbar(aes(ymin = meanVol - seV,  ymax = meanVol + seV), position = position_dodge(0.5)) +
+  theme_minimal()
