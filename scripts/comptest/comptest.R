bf1fe4b8185038ba5b1a1fdd634b4a4f0c7d0a9f
grade0ne <alex.mendelson.698@my.csun.edu>
2025-09-12 11:40:54 -0700

    comptest d2

--- scripts/comptest/comptest.R
+++ scripts/comptest/comptest.R
@@ -2,4 +2,17 @@ packages <- c('tidyverse', 'growthrates')
 lapply(packages, library, character.only=TRUE)
 
 data <- read.csv("data/comptest/comptestdata.csv")
-data$treatment <- factor(data$treatment)
+data[c('treatment', 'rep', 'sp')] <- lapply(data[c('treatment', 'rep', 'sp')], factor)
+
+graphdata <- data %>%
+  filter(day == 2) %>%
+  group_by(treatment, sp) %>%
+  summarize(mean = mean(count),
+            n = length(count),
+            se = sd(count) / sqrt(n))
+
+ggplot(graphdata, aes(x = treatment, y = mean, fill = sp)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_errorbar(stat = "identity", position = position_dodge(), aes(ymax = mean + se, ymin = mean - se)) +
+  facet_wrap(~sp) +
+  theme_minimal()
