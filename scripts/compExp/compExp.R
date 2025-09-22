ea74f329c04ee4eddddb35469244c4c0c1826f26
grade0ne <alex.mendelson.698@my.csun.edu>
2025-09-22 16:12:01 -0700

    minor compexp script edits

--- scripts/compExp/compExp.R
+++ scripts/compExp/compExp.R
@@ -1,4 +1,5 @@
-lbs<-c('tidyverse','car','moments')
+lbs<-c('tidyverse','car','moments','lme4','lmerTest')
 lapply(lbs,library,character.only=TRUE)
-
-rawData <- read.csv("data/compexp/compexpData.csv")
+rawData<-read.csv("data/compexp/compexpData.csv") %>%
+  mutate(across(c(replicate,evolvedTemp,currentTemp,competition,species,idCode),
+                as.factor))
