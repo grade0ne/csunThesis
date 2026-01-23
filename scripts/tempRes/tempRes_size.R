291a4d9a9235ccb2cc7ffdf0fb3dc7a4eb75aabf
unknown <alex.mendelson.698@my.csun.edu>
2026-01-22 20:14:22 -0800

    thesis manuscript methods updates, tempres main script v1

--- scripts/tempRes/tempRes_size.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-data <- read.csv("data/experiment 2/size_data/example_size_data_format.csv")
-
-data <- data %>%
-  separate(ID, into = c("CultureID", "ImageNum", "PostureCode", "temp1", "temp2", "ROINum"), sep = "_", convert = TRUE)
-
-  
--- scripts/z_archive/apcola_map.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-library(sf)
-library(leaflet)
-
-apalachacola_sf <- st_read(data/S_USA.AdministrativeForest.shp)
-
-st_layers(data/S_USA.AdministrativeForest.shp)
