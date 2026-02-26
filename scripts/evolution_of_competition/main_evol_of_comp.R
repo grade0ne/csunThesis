ec3baf909c0ccd15fd97c9fec375a145773d5c0d
grade0ne <alex.mendelson.698@my.csun.edu>
2026-02-26 15:15:35 -0800

    idk a lot

--- scripts/evolution_of_competition/main_evol_of_comp.R
+++ scripts/evolution_of_competition/main_evol_of_comp.R
@@ -3,9 +3,8 @@ library(tidyverse)
 #### DATA HANDLING ####
 
 popData <- read.csv("data/compexp/compexpData.csv") %>%
-  mutate(across(c(day, count), as.integer),
-         across(c(evolvedTemp, currentTemp, competition, repNum), factor)) %>%
-  select(day, species, evolvedTemp, currentTemp, competition, repNum, count)
+  mutate(hour = as.integer(day * 24),
+         across(c(evolvedTemp, currentTemp, competition, repNum), factor))
 
 rPopData <- popData %>%
   filter(species == "rotifer")
@@ -18,15 +17,20 @@ pPopData <- popData %>%
 
 library(growthrates)
 
-# Rotifer parameters:
-INIT_PARAMS <- c(y0 = 1, r = 0.3, alpha = 0.15)
-LOWER_PARAMS <- c(y0 = 0, r = 0.003, alpha = 0.003)
-UPPER_PARAMS <- c(y0 = 10, r = 8, alpha = 1)
+# Rotifer parameters: (TIME IN HOURS)
+INIT_PARAMS <- c(y0 = 5, r = 0.1, alpha = 0.0005)
+LOWER_PARAMS <- c(y0 = 3, r = 0.003, alpha = 0.0001)
+UPPER_PARAMS <- c(y0 = 7, r = 8, alpha = 1)
 
 # Protist parameters:
 INIT_PARAMS <- c(y0 = 10, r = 0.3, alpha = 0.15)
 LOWER_PARAMS <- c(y0 = 0, r = 0.003, alpha = 0.001)
-UPPER_PARAMS <- c(y0 = 80, r = 10, alpha = 1)
+UPPER_PARAMS <- c(y0 = 80, r = 20, alpha = 1)
+
+# Protist parameters: (TIME IN HOURS)
+INIT_PARAMS <- c(y0 = 10, r = 0.1, alpha = 0.0003)
+LOWER_PARAMS <- c(y0 = 5, r = 0.001, alpha = 0.000005)
+UPPER_PARAMS <- c(y0 = 15, r = 5, alpha = 0.02)
 
 # Defining custom function per Part 2, sec. 4 of growthrates documentation
 logistic_alpha_function <- function(time, parms) {
@@ -49,7 +53,7 @@ grow_logistic_alpha <- growthmodel(
 # usage: 'model_object_name' <- fit_models()
 fit_models <- function(data, initial = INIT_PARAMS, lower = LOWER_PARAMS, upper = UPPER_PARAMS) {
   all_growthmodels(
-    count ~ day | evolvedTemp + currentTemp + competition + repNum, # CUSTOMIZE TO DATA SET
+    count ~ hour | evolvedTemp + currentTemp + competition + repNum, # CUSTOMIZE TO DATA SET
     data = data, # CUSTOMIZE TO DATA SET
     p = initial,
     upper = upper,
@@ -83,58 +87,82 @@ plot_models <- function(models) {
   par(mfrow = c(1, 1))
 }
 
-# model summaries
-summarize_models <- function(models) {
-  
-  ids <- rownames(coef(models))
-  out_list <- list()
-  
-  for (i in seq_along(ids)) {
-    fit_id <- ids[i]
-    fit <- models[[fit_id]]
-    
-    summary <- summary(fit)
-    table <- as.data.frame(summary$par)
-    
-    table$term <- rownames(table)
-    table$model_id <- fit_id
-    rownames(table) <- NULL
-    
-    out_list[[i]] <- table
-  }
-  
-  out <- do.call(rbind, out_list)
-  rownames(out) <- NULL
-  out
-}
 
 #### ANOVA ####
 
-rGrowData <- results(models) %>%
+rGrowData <- results(r_models) %>%
   rownames_to_column(var = "id") %>%
   separate(col = "id", into = c("evolvedTemp", "currentTemp", "competition", "replicate"), sep = ":") %>%
   mutate(across(c(evolvedTemp, currentTemp, competition, replicate), factor)) %>%
-  select(!repNum)
+  mutate(r_hour = r,
+         r_day = r * 24,
+         alpha_hour = alpha,
+         alpha_day = alpha * 24,
+         K = r_day / alpha_day)
 
-pGrowData <- results(models) %>%
+pGrowData <- results(p_models) %>%
   rownames_to_column(var = "id") %>%
   separate(col = "id", into = c("evolvedTemp", "currentTemp", "competition", "replicate"), sep = ":") %>%
   mutate(across(c(evolvedTemp, currentTemp, competition, replicate), factor),
          compFactor = case_when(evolvedTemp == "25" & competition == TRUE ~ "25-evol-rotif",
                                 evolvedTemp == "30" & competition == TRUE ~ "30-evol-rotif",
-                                competition == FALSE ~ "no-comp"))
+                                competition == FALSE ~ "no-comp")) %>%
+  mutate(r_hour = r,
+         r_day = r * 24,
+         alpha_hour = alpha,
+         alpha_day = alpha * 24)
 
 library(car)
 library(MASS)
 library(moments)
+library(emmeans)
 
-model1 <- lm(log(r) ~ evolvedTemp * currentTemp * competition, data = rGrowData)
+# Rotifer r
+model1 <- lm(log(r_day) ~ evolvedTemp * currentTemp * competition, data = rGrowData)
+Anova(model1, type = "III")
 
-model2 <- lm(log(alpha) ~ evolvedTemp * currentTemp * competition, data = rGrowData)
+emm1 <- emmeans(model1, ~ evolvedTemp | competition)
+pairs(emm1, adjust = "tukey")
 
-model3 <- lm(log(r) ~ currentTemp * compFactor, data = pGrowData)
 
-model4 <- lm(log(alpha) ~ currentTemp * compFactor, data = pGrowData)
+# Rotifer a
+model2 <- lm(log(alpha_day) ~ evolvedTemp * currentTemp * competition, data = rGrowData)
+Anova(model2, type = "III")
+
+emm2 <- emmeans(model2, ~ competition | evolvedTemp)
+pairs(emm2, adjust = "tukey")
+
+
+# Protist r
+model3 <- lm(log(r_day) ~ currentTemp * compFactor, data = pGrowData)
+Anova(model3, type = "III")
+
+emm3 <- emmeans(model3, ~ compFactor | currentTemp)
+pairs(emm3, adjust = "tukey")
+
+
+# Protist a
+model4 <- lm(log(alpha_day) ~ currentTemp * compFactor, data = pGrowData)
+Anova(model4, type = "III")
+
+emm4 <- emmeans(model4, ~ compFactor | currentTemp)
+pairs(emm4, adjust = "tukey")
+
+
 #### FIGURES ####
+
+fig1data <- rGrowData %>%
+  group_by(evolvedTemp, currentTemp, competition) %>%
+  summarize(mean_r = mean(r_day), ci_r = 1.96 * sd(r_day)/sqrt(length(r_day)))
+
+ggplot(fig1data, aes(x = currentTemp, y = mean_r, fill = competition)) +
+  geom_bar(stat = "identity", position= position_dodge(width = 1)) +
+  geom_errorbar(stat = "identity", position = position_dodge(width = 1),
+                aes(ymin = mean_r - ci_r, ymax = mean_r + ci_r)) +
+  facet_wrap(~evolvedTemp) +
+  theme_minimal()
+
+
+
 
 
